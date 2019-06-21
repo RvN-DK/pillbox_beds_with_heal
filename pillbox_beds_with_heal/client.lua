b841bd455e625ac3bbb62d1093fbefa69339c2fa
RvN-DK <52079779+RvN-DK@users.noreply.github.com>
2019-06-21 22:20:29 +0200

    Add files via upload

---
+++ pillbox_beds_with_heal/client.lua
@@ -0,0 +1,100 @@
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+
+ESX = nil
+
+local InAction = false
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+
+        Citizen.Wait(5)
+
+        for i=1, #Config.BedList do
+            local bedID   = Config.BedList[i]
+            local distance = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), bedID.objCoords.x, bedID.objCoords.y, bedID.objCoords.z, true)
+
+            if distance < Config.MaxDistance and InAction == false then
+
+                ESX.Game.Utils.DrawText3D({ x = bedID.objCoords.x, y = bedID.objCoords.y, z = bedID.objCoords.z + 1 }, bedID.text, 0.6)
+
+                if IsControlJustReleased(0, Keys['E']) then
+                    bedActive(bedID.objCoords.x, bedID.objCoords.y, bedID.objCoords.z, bedID.heading, bedID)
+                end
+            end
+        end
+    end
+end)
+
+function bedActive(x, y, z, heading)
+
+    SetEntityCoords(GetPlayerPed(-1), x, y, z + 0.3)
+    RequestAnimDict('missfbi5ig_0')
+    while not HasAnimDictLoaded('missfbi5ig_0') do
+        Citizen.Wait(0)
+    end
+    TaskPlayAnim(GetPlayerPed(-1), 'missfbi5ig_0' , 'lyinginpain_loop_steve' ,8.0, -8.0, -1, 1, 0, false, false, false )
+    SetEntityHeading(GetPlayerPed(-1), heading + 180.0)
+    InAction = true
+
+    Citizen.CreateThread(function ()
+        Citizen.Wait(5)
+        local player = GetPlayerPed(-1)
+
+        if (GetEntityHealth(GetPlayerPed(-1)) < 200)  then
+            TriggerEvent('esx:showNotification', '~g~You are being treated, please wait'); 
+            Citizen.Wait(5000) -- Time before heal
+            SetEntityHealth(PlayerPedId(-1), 200)
+            TriggerEvent('esx:showNotification', '~g~ You are now healthy'); 
+
+        elseif (GetEntityHealth(GetPlayerPed(-1)) == 200) then
+            TriggerEvent('esx:showNotification', '~y~ You do not need medical attention'); 
+        end
+    end)
+
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+
+            if InAction == true then
+                DrawSub('Press ~g~X~w~ to get back up')
+                if IsControlJustReleased(0, Keys['X']) then
+                    ClearPedTasks(GetPlayerPed(-1))
+                    FreezeEntityPosition(GetPlayerPed(-1), false)
+                    SetEntityCoords(GetPlayerPed(-1), x + 1.0, y, z)
+                    InAction = false
+                end
+            end
+        end
+    end)
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+    end
+end)
+
+function DrawSub(msg, time)
+    ClearPrints()
+    SetTextEntry_2("STRING")
+    AddTextComponentString(msg)
+    DrawSubtitleTimed(time, 1)
+end
---
+++ pillbox_beds_with_heal/config.lua
@@ -0,0 +1,77 @@
+Config              = {}
+Config.MaxDistance = 1.5
+
+Config.BedList = {
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 356.73, y = -585.71, z = 43.11},---pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 360.51, y = -586.66, z = 43.11},--pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 353.12, y = -584.66, z = 43.50},---pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 349.62, y = -583.53, z = 43.022},---pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 80.0,
+		objCoords  = {x = 344.80, y = -581.12, z = 43.02},---pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 80.0,
+		objCoords  = {x = 334.09, y = -578.43, z = 43.01}, --pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 323.64, y = -575.16, z = 43.02}, --pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = -20.0,
+		objCoords  = {x = 326.97, y = -576.229, z = 43.02}, --pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 160.0,
+		objCoords  = {x = 354.24, y = -592.74, z = 43.11}, --pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 160.0,
+		objCoords  = {x = 357.34, y = -594.45, z = 43.11}, --pillbox bed
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 160.0,
+		objCoords  = {x = 350.80, y = -591.72, z = 43.11}, --pillbox bed 
+	},
+
+	{
+		text = '[E] Lie on the bed',
+		heading = 160.0,
+		objCoords  = {x = 346.89, y = -591.01, z = 42.58},   --pillbox bed 
+	}
+}
